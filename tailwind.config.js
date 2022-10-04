fd9894020d40d5d03821df481fd59cf0bc53352f
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-10-03 21:33:35 -0400

    initial commit next + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	darkMode: "media",
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Rubik", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};

616b406f5a6afed47ae2d9848d6c67e665a8fbac
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-11-08 01:01:10 -0500

    add star background

---
+++ components/Background.tsx
@@ -0,0 +1,72 @@
+import { animate } from "motion";
+import React, { useEffect } from "react";
+import * as THREE from "three";
+
+export default function Background() {
+	useEffect(() => {
+		// 3D three.js space worp
+		const canvas = document.getElementById(
+			"threejscanvas"
+		) as HTMLCanvasElement;
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			60,
+			window.innerWidth / window.innerHeight,
+			1,
+			1000
+		);
+		camera.position.z = 1;
+		camera.position.x = Math.PI / 2;
+
+		let rendender = new THREE.WebGLRenderer();
+		rendender.setSize(window.innerWidth, window.innerHeight);
+		canvas.appendChild(rendender.domElement);
+
+		let starGeometry = new THREE.BufferGeometry();
+		let points: THREE.Vector3[] = [];
+		let speeds: { vel: number; acc: number }[] = [];
+		for (let i = 0; i < 2000; i++) {
+			let star = new THREE.Vector3();
+			star.x = THREE.MathUtils.randFloatSpread(600);
+			star.y = THREE.MathUtils.randFloatSpread(600);
+			star.z = THREE.MathUtils.randFloatSpread(600);
+			speeds.push({
+				vel: 0,
+				acc: 0.02,
+			});
+			points.push(star);
+		}
+		starGeometry.setFromPoints(points);
+
+		let sprite = new THREE.TextureLoader().load("/assets/star.png");
+		let starMaterial = new THREE.PointsMaterial({
+			color: 0xaaaaaa,
+			size: 0.7,
+			map: sprite,
+		});
+
+		let stars = new THREE.Points(starGeometry, starMaterial);
+		scene.add(stars);
+
+		const animate = () => {
+			points.forEach((point, i) => {
+				speeds[i].vel += speeds[i].acc;
+				point.y -= speeds[i].vel;
+				if (point.y < -200) {
+					point.y = 200;
+					speeds[i].vel = 0;
+				}
+			});
+			starGeometry.setFromPoints(points);
+			stars.rotation.y += 0.002;
+			stars.rotation.x = -1.5;
+
+			rendender.render(scene, camera);
+			requestAnimationFrame(animate);
+		};
+
+		animate();
+	}, []);
+
+	return <div className="fixed -z-50" id="threejscanvas" />;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import "../styles/globals.css";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import Background from "../components/Background";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 	return (
 		<div className="h-full flex flex-col justify-between">
+			<Background />
 			<div className="flex justify-center fixed pl-3 md:pl-3 pt-3 md:pt-0 bottom-5 md:top-3 z-50">
 				<Nav />
 			</div>

a30db27a93bc16bc111f4834d40b423aac535a9a
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-11-08 18:35:16 -0500

    add favicon and meta stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/globals.css";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Background from "../components/Background";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
@@ -12,6 +13,24 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 	return (
 		<div className="h-full flex flex-col justify-between">
+			<Head>
+				<title>BlairHacks</title>
+				<link rel="icon" type="image/x-icon" href="/assets/logo.png" />
+
+				<meta
+					name="description"
+					content="An annual hackathon hosted by MBHS students in Silver Spring, MD."
+				/>
+				<meta
+					property="og:description"
+					content="An annual hackathon hosted by MBHS students in Silver Spring, MD."
+					key="ogdesc"
+				/>
+				<meta property="og:image" content="/assets/logo.png" key="ogimage" />
+				<meta property="og:title" content="BlairHacks" key="ogtitle" />
+				<meta property="og:site_name" content="BlairHacks" key="ogsitename" />
+				<meta name="theme-color" content="#000000" />
+			</Head>
 			<Background />
 			<div className="flex justify-center fixed pl-3 md:pl-3 pt-3 md:pt-0 bottom-5 md:top-3 z-50">
 				<Nav />

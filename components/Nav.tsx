62cafb62bd99f5e3e531f47d0bdb3e5a7cedc39b
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-11-15 20:41:18 -0500

    get rid of replit stuff

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,31 +6,31 @@ import { BiDonateHeart } from "react-icons/bi";
 import { useRouter } from "next/router";
 
 export default function Nav() {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div className="w-fit">
-      <div className="flex md:flex-col gap-2 p-2 text-neutral-500 rounded-lg bg-white bg-opacity-5 border-2 border-neutral-900 backdrop-blur-sm">
-        <img className="w-12" src="/assets/logo.png" alt="BlairHacks Logo" />
-        <Link href="/">
-          <HiOutlineHome
-            color={router.pathname === "/" ? "white" : undefined}
-            className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
-          />
-        </Link>
-        <Link href="/team">
-          <AiOutlineTeam
-            color={router.pathname === "/team" ? "white" : undefined}
-            className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
-          />
-        </Link>
-        <Link href="/sponsors">
-          <BiDonateHeart
-            color={router.pathname === "/sponsors" ? "white" : undefined}
-            className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
-          />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-fit">
+			<div className="flex md:flex-col gap-2 p-2 text-neutral-500 rounded-lg bg-white bg-opacity-5 border-2 border-neutral-900 backdrop-blur-sm">
+				<img className="w-12" src="/assets/logo.png" alt="BlairHacks Logo" />
+				<Link href="/">
+					<HiOutlineHome
+						color={router.pathname === "/" ? "white" : undefined}
+						className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
+					/>
+				</Link>
+				<Link href="/team">
+					<AiOutlineTeam
+						color={router.pathname === "/team" ? "white" : undefined}
+						className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
+					/>
+				</Link>
+				<Link href="/sponsors">
+					<BiDonateHeart
+						color={router.pathname === "/sponsors" ? "white" : undefined}
+						className="h-12 w-12 p-2 rounded-lg hover:bg-opacity-50 hover:bg-neutral-800 sm:cursor-pointer"
+					/>
+				</Link>
+			</div>
+		</div>
+	);
 }
